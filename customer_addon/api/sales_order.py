ecc3b0e7b222853eb87a8ddcdc94ac9e713c716f
Sukeshanee <zende.sukeshanee@kgce.edu.in>
2026-07-02 05:43:01 +0000

    Changed Feild name Only

--- customer_addon/api/sales_order.py
+++ customer_addon/api/sales_order.py
@@ -1,3 +1,72 @@
+# import frappe
+# import json
+
+
+# @frappe.whitelist()
+# def get_customer_assets(customer, filters=None):
+
+#     # -----------------------------
+#     # Safe JSON parsing
+#     # -----------------------------
+#     if isinstance(filters, str):
+#         try:
+#             filters = json.loads(filters)
+#         except Exception:
+#             filters = {}
+
+#     filters = filters or {}
+
+#     # -----------------------------
+#     # Base condition (important)
+#     # -----------------------------
+#     conditions = {
+#         "parent": customer
+#     }
+
+#     # -----------------------------
+#     # Multi-select Location support
+#     # -----------------------------
+#     if filters.get("location"):
+#         conditions["location"] = ["in", filters.get("location")]
+
+#     # -----------------------------
+#     # Normal filters
+#     # -----------------------------
+#     for key in [
+#         "sub_location",
+#         "custom_item_group",
+#         "custom_item_code",
+#         "model",
+#         "type"
+#     ]:
+#         if filters.get(key):
+#             conditions[key] = filters.get(key)
+
+#     # -----------------------------
+#     # Fetch records
+#     # -----------------------------
+#     return frappe.get_all(
+#         "Customer Installed Asset",
+#         filters=conditions,
+#         fields=[
+#             "location",
+#             "sub_location",
+#             "sub_location_address",
+#             "custom_item_group",
+#             "custom_item_code",
+#             "model",
+#             "type",
+#             "amc",
+#             "start_date",
+#             "end_date",
+#             "warrenty"
+#         ],
+#         limit_page_length=1000
+#     )
+
+
+
+
 import frappe
 import json
 
@@ -5,9 +74,6 @@ import json
 @frappe.whitelist()
 def get_customer_assets(customer, filters=None):
 
-    # -----------------------------
-    # Safe JSON parsing
-    # -----------------------------
     if isinstance(filters, str):
         try:
             filters = json.loads(filters)
@@ -16,24 +82,14 @@ def get_customer_assets(customer, filters=None):
 
     filters = filters or {}
 
-    # -----------------------------
-    # Base condition (important)
-    # -----------------------------
     conditions = {
         "parent": customer
     }
 
-    # -----------------------------
-    # Multi-select Location support
-    # -----------------------------
     if filters.get("location"):
         conditions["location"] = ["in", filters.get("location")]
 
-    # -----------------------------
-    # Normal filters
-    # -----------------------------
     for key in [
-        "sub_location",
         "custom_item_group",
         "custom_item_code",
         "model",
@@ -42,16 +98,12 @@ def get_customer_assets(customer, filters=None):
         if filters.get(key):
             conditions[key] = filters.get(key)
 
-    # -----------------------------
-    # Fetch records
-    # -----------------------------
     return frappe.get_all(
         "Customer Installed Asset",
         filters=conditions,
         fields=[
             "location",
-            "sub_location",
-            "sub_location_address",
+            "address",
             "custom_item_group",
             "custom_item_code",
             "model",
